d587413c1432c22285a311c92c367f43e5373050
Alex <alexoliveira604@gmail.com>
2021-06-17 16:57:50 -0300

    Migration from React App to Next App

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
-// tailwind.config.js
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    container: {
-      center: true,
-    }
+    extend: {},
   },
   variants: {
     extend: {},
